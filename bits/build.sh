56bd99ab26274110e8f3ca95884f6aa7df20b92c
Catalin Istratoiu <catalin@istratoiu.com>
2022-06-26 14:09:23 +0300

    Blog Actualizat: 2022-06-26 14:09:23

--- bits/build.sh
+++ bits/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cat header.htm menu.htm content.htm blog.htm footer.htm > ../index.html
+cat header.htm content.htm blog.htm footer.htm > ../index.html
 cd ..
 git add .
 git commit -m "Blog Actualizat: `date +'%Y-%m-%d %H:%M:%S'`"
